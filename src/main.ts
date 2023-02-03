ecef92891e0026b4b017833802596f7d32f1cc13
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2023-02-03 16:05:03 -0300

    feat: Telegram Bot integration

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,18 @@
 import { ofetch } from 'ofetch';
-import * as dotenv from 'dotenv';
+import TelegramBot from 'node-telegram-bot-api';
 import { IPost, ISubmission } from './types';
+import * as dotenv from 'dotenv';
 dotenv.config();
 
 /* eslint-disable-next-line @typescript-eslint/no-var-requires */
 const Cache = require('persistent-cache');
 const cache = Cache({ base: '.cache', duration: 86400 });
 
-const baseURL = 'https://oauth.reddit.com/api/v1/';
-
 async function redditAuth(): Promise<string> {
   const cachedToken = cache.getSync('access_token');
   if (cachedToken !== undefined) return cachedToken;
 
   const { access_token: token } = await ofetch('https://www.reddit.com/api/v1/access_token', {
-    baseURL,
     method: 'POST',
     query: {
       grant_type: 'password',
@@ -40,14 +38,16 @@ async function redditAuth(): Promise<string> {
 (async function () {
   const token = await redditAuth();
 
-  const { data: submissions } = await ofetch(`/r/${process.env.REDDIT_SUBREDDIT}/hot`, {
-    baseURL: 'https://oauth.reddit.com/',
-    method: 'GET',
-    query: { limit: '5' },
-    headers: {
-      Authorization: `Bearer ${token}`
+  const { data: submissions } = await ofetch(
+    `https://oauth.reddit.com/r/${process.env.REDDIT_SUBREDDIT}/hot`,
+    {
+      method: 'GET',
+      query: { limit: '10' },
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
     }
-  });
+  );
 
   const posts: Array<IPost> = [];
 
@@ -55,12 +55,12 @@ async function redditAuth(): Promise<string> {
     const { data } = post;
 
     if (data?.is_gallery || data?.post_hint !== 'image') {
-      console.log('Tipo de post não suportado');
+      console.error('Tipo de post não suportado');
       return;
     }
 
     if (data.url === undefined) {
-      console.log('Post sem URL');
+      console.error('Post sem URL');
       return;
     }
 
@@ -80,7 +80,7 @@ async function redditAuth(): Promise<string> {
         break;
 
       default:
-        console.log('Tipo de mídia não suportada');
+        console.error('Tipo de mídia não suportada');
         return;
     }
 
@@ -92,7 +92,29 @@ async function redditAuth(): Promise<string> {
     });
   });
 
-  console.log(posts);
+  if (process.env.TELEGRAM_API_TOKEN === undefined || process.env.TELEGRAM_CHAT_ID === undefined)
+    process.exit(1);
+
+  const apiToken = process.env.TELEGRAM_API_TOKEN;
+  const chatId = process.env.TELEGRAM_CHAT_ID;
+
+  const bot = new TelegramBot(apiToken, { polling: true });
+
+  for (const post of posts) {
+    const method = post.type === 'image/gif' ? 'sendDocument' : 'sendPhoto';
+
+    try {
+      await bot[method](chatId, post.media, {
+        caption: `[Profile](${post.author}) · [Source](${post.source}) ${chatId}`,
+        parse_mode: 'MarkdownV2'
+      });
+    } catch (err: any) {
+      console.error(err.response);
+      console.log(err.constructor.name);
+    }
+  }
+
+  process.exit();
 })();
 
 function getUrlExtension(url: string): string {
